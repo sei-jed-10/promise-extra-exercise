632aa23e4bd6e7bf462f0917f092848fb4f6e044
daghustani <57670752+daghustani@users.noreply.github.com>
2020-01-05 10:48:38 +0300

    Update hw.js

--- hw.js
+++ hw.js
@@ -8,30 +8,7 @@ const db = require('./order')
 
 //CODE FOR QUESTION 1 HERE
 
-db.readMenu()
-.then(success => {
-  let newlineSplit = success.split("\n") // split at new line
-  // console.log(newlineSplit)
-  let items = newlineSplit.splice(3,14) // remove 1st 3 elements
 
-  // console.log(items)
-  let price = items[0].split(":")[1] //split to seperate price from name
-
-
-  // let index = 0
-
-  // //find the price with cheapest
-  items.forEach((element, i) => {
-    console.log(element.split(":"))
-    if(price > element.split(":")[1]){
-        price = element.split(":")[1]
-        index = i
-    }
-  });
-
-  console.log("Cheapest Burger: ",`${items[index]}`);
-
-  }).catch(err => console.log(err) )
 
 
 // Order Some Food
@@ -41,9 +18,7 @@ db.readMenu()
 // Once the promise has been resolved, console.log "Man that burger sure was tasty!".
 
 // CODE FOR QUESTION 2 HERE
-db.order("BlUe 'sHrOOM")
-.then(success => console.log(success))
-.catch(err => console.log(err))
+
 
 
 // 3. Try and use the same method to order a `quesadilla`.
@@ -51,9 +26,8 @@ db.order("BlUe 'sHrOOM")
 //  Make sure that your Promise can be dealt with when it is rejected.
 
 // CODE FOR QUESTION 3 HERE
-db.order("quesadilla")
-.then(success => console.log(success))
-.catch(err => console.log(err))
+
+
 
 // Add A Burger To The Menu
 /* *************************************************************** */
@@ -64,14 +38,7 @@ db.order("quesadilla")
 //  Add a method to console.log the response once the promise is resolved!
 //  Also add a method to catch an error if something goes wrong
 
-const newBurger = {
-    "name": "TRIPLE CORONARY BYPASS",
-    "price": 28.95,
-    "description": ` It becomes difficult to even describe this, the most preposterous of our Bypass Burgers. All you really need to know is that we use three burger patties, three fried eggs, fourteen slices of American cheese, and ten slices of bacon, all packed between two grilled cheese sandwiches. Figuring out how to add condiments is completely up to you. It’s served in a big bowl of french fries and tater tots covered, in lots of our Cheesy-Cheese Goo. Enjoy!`
-  }
-  db.addToMenu(newBurger)
-  .then(success => console.log(success))
-  .catch(err => console.log(err))
+
 
   // CODE FOR QUESTION 4 HERE
 
@@ -79,11 +46,6 @@ const newBurger = {
   // We only want to call `readMenu` after we get a response that is successful.
   // Remember that we can chain promises together the same way that we can chain jQuery methods.
   // db.addToMenu(newBurger)
-  db.addToMenu(newBurger)
-  .then(success =>{
-  db.readMenu()
-  .then(success => console.log(success))})
-  .catch(err => console.log(err))
 
   // REFACTOR QUESTION 4 TO ACCOUNT FOR THIS REQUIREMENT
 
@@ -113,10 +75,3 @@ const newBurger = {
   ]
 
   // CODE FOR QUESTION 6 HERE
-  let orders = []
-  customers.forEach(el => {
-    orders.push(db.order(el.order)) //store all promises in array
-  })
-
-  Promise.all(orders) //takes arrays and resolves at once
-  .then(suc => console.log(suc))
